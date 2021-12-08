32bc08d4e8ced150b3cd8a45751cdab2afd462f2
Kurihara Jun <lh182051+src@gmail.com>
2021-12-09 02:31:37 +0900

    Add read benchmark

--- benchmark-io/FileIOTest.cs
+++ benchmark-io/FileIOTest.cs
@@ -7,6 +7,8 @@ using System;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
+using System.Buffers;
+using System.Text;
 
 namespace Benchmark.IO
 {
@@ -52,6 +54,59 @@ namespace Benchmark.IO
 
         private byte[] writeData = new byte[8_000];
 
+        private byte[] binaryData = new byte[]
+        {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09
+        };
+
+        private string textData = "abcdefghij";
+
+        private string textFilePath = Path.GetTempFileName();
+
+        private string binaryFilePath = Path.GetTempFileName();
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Task.WaitAll(
+                Task.Run(async () =>
+                {
+                    using (var fs = new FileStream(binaryFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 64, true))
+                    {
+                        for (int i = 0; i < 100_000_000 / 10; i++)
+                        {
+                            await fs.WriteAsync(binaryData);
+                        }
+                    }
+                }),
+                Task.Run(async () =>
+                {
+                    using (var fs = new FileStream(textFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 64, true))
+                    using (var st = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        for (int i = 0; i < 100_000_000 / 10; i++)
+                        {
+                            await st.WriteAsync(textData);
+                        }
+                    }
+                })
+            );
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            if (File.Exists(textFilePath))
+            {
+                File.Delete(textFilePath);
+            }
+
+            if (File.Exists(binaryFilePath))
+            {
+                File.Delete(binaryFilePath);
+            }
+        }
+
         [IterationSetup]
         public void IterationSetup()
         {
@@ -71,7 +126,7 @@ namespace Benchmark.IO
         }
 
         [Benchmark]
-        public async Task WriteAsyncOnceAsync()
+        public async Task WriteAsync()
         {
             using (var fs = new FileStream(Path.Join(tempDir, "1.txt"), FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 1, FileOptions.Asynchronous))
             {
@@ -83,7 +138,7 @@ namespace Benchmark.IO
         }
 
         [Benchmark]
-        public void WriteOnce()
+        public void WriteSync()
         {
             using (var fs = new FileStream(Path.Join(tempDir, "2.txt"), FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, 1, FileOptions.None))
             {
@@ -91,6 +146,26 @@ namespace Benchmark.IO
                 {
                     fs.Write(writeData);
                 }
+            }
+        }
+
+        [Benchmark]
+        public async Task ReadBinaryAsync()
+        {
+            using (var fs = new FileStream(binaryFilePath, FileMode.Open, FileAccess.Read, FileShare.None, 1, FileOptions.Asynchronous))
+            using (var buffer = MemoryPool<byte>.Shared.Rent())
+            {
+                await fs.ReadAsync(buffer.Memory);
+            }
+        }
+
+        [Benchmark]
+        public async Task ReadToEndAsync()
+        {
+            using (var fs = new FileStream(textFilePath, FileMode.Open, FileAccess.Read, FileShare.None, 1, FileOptions.Asynchronous))
+            using (var reader = new StreamReader(fs, System.Text.Encoding.UTF8))
+            {
+                await reader.ReadToEndAsync();
             }
         }
     }
